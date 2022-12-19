da8aeecdde4478113be06587de3eb046dd81669b
0xosamaa <0xm.osamaa@gmail.com>
2022-12-19 05:37:02 +0200

    slide up to game screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,10 @@ import { Bird } from './Bird.js';
 
 const go_button = document.querySelector('button#go');
 const name_input = document.querySelector('input[name=name]');
-const start_items = document.querySelector('.container');
+const enter_name = document.querySelector('.enter-name');
+const instructions = document.querySelector('.instructions');
+const container = document.querySelector('.container');
+const game_container = document.querySelector('.game-container');
 
 let player = 'Player 1';
 let timer = 60;
@@ -12,7 +15,17 @@ let birds_killed = 0;
 go_button.addEventListener('click', () => {
     if (name_input.value) {
         player = name_input.value;
-        start_items.style.display = 'none';
+        enter_name.style.display = 'none';
+        instructions.style.display = 'none';
+        game_container.style.display = 'flex';
+        let x = 0;
+        const id = setInterval(() => {
+            x -= 5;
+            container.style.transform = `translateY(${x}%)`;
+            game_container.style.transform = `translateY(${x}%)`;
+            if (x == -100) {
+                clearInterval(id);
+            }
+        }, 100);
     }
 });
-
